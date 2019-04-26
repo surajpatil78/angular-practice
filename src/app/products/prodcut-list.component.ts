fbeb2ba0609952ed18e920b15612c8450f29ab6f
surajpatil78 <patilsuraj2210@gmail.com>
2019-04-26 18:21:51 +0530

    3 files modified

--- src/app/products/prodcut-list.component.ts
+++ src/app/products/prodcut-list.component.ts
@@ -1,9 +1,22 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
-  selector:'pm-products',
-  templateUrl:'./product-list.component.html'
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
 })
 
-export class ProductListComponent{
-
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth:number=50;
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "abc",
+      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+    },
+    {
+      "productId": 5,
+      "productName": "xyz",
+      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png"
+    }
+  ]
 }
