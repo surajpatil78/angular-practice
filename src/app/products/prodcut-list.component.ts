2807d49023fcf3ea4a4e538a08af59c1a370b0ed
surajpatil78 <patilsuraj2210@gmail.com>
2019-04-29 18:08:48 +0530

    2 files modified, 2 added

--- src/app/products/prodcut-list.component.ts
+++ src/app/products/prodcut-list.component.ts
@@ -1,22 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls:['./products/product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent
+  implements OnInit {
   pageTitle: string = 'Product List';
-  imageWidth:number=50;
-  products: any[] = [
+  imageWidth: number = 50;
+  showImage: boolean = false;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this._listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  filteredProducts: IProduct[];
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  constructor(){
+    this.filteredProducts=this.products;
+    this._listFilter='cart';
+  }
+
+  products: IProduct[] = [
     {
       "productId": 2,
       "productName": "abc",
-      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+      "productRating":3
     },
     {
       "productId": 5,
       "productName": "xyz",
-      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png"
+      "imageUrl": "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png",
+      "productRating":4
     }
   ]
+
+  ToggelImages(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit():void{
+    console.log('In OnInit');
+  }
+
 }
