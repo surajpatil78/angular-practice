537e476bfb917e63de396f7006a5c8b71b4f2ea1
surajpatil78 <patilsuraj2210@gmail.com>
2019-05-03 17:58:12 +0530

    send and get route parameter (url)

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,16 +1,21 @@
 
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
-  templateUrl:'./product-detail.component.html',
-  styleUrls:['./product-detail.component.css']
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
 })
 
-export class ProductDetailsComponent implements OnInit{
-  pageTitle:string = 'Product Detail';
+export class ProductDetailsComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
 
-  ngOnInit(){
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    console.log('id : ' + id);
   }
-  
+
 }
