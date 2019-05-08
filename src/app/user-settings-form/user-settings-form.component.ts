b436922cf649493949f73107429f688fcdbf9811
surajpatil78 <patilsuraj2210@gmail.com>
2019-05-08 11:46:57 +0530

    3 files modified

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,9 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IUserSettings} from './data/user-settings';
+
 @Component({
   selector:'user-settings-form',
   templateUrl:'./user-settings-form.component.html'
 })
 
-export class UserSettingsFormComponent{
-  
+export class UserSettingsFormComponent 
+    implements OnInit{
+
+ constructor(){}
+
+userSettings:IUserSettings={
+  name:'Milton'
+}
+
+  ngOnInit() {
+
+  }
+
 }
