8e0a3ca62a84bb280a1710b412c0049abd56516c
surajpatil78 <patilsuraj2210@gmail.com>
2019-09-16 18:48:20 +0530

    ngFor with block and various conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = '';
+export class AppComponent {
+
+  courses=[
+    {id:1, name:'Angular'},
+    {id:2, name:'Dot Net'},
+    {id:3, name:'C#'}
+  ];
+  
 }
   
